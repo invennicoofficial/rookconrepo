a0809470b745caa2427a7ae2b4d5b322ce96b1a2
Dayana <dayanapatel@freshfocusmedia.com>
2018-06-19 15:31:06 -0700

    Ticket #7537 - Ticket Security: Action View vs Full View

--- db_data_dayana.php
+++ db_data_dayana.php
@@ -4,7 +4,15 @@
     //Dayana's Database Changes
     echo "Dayana's DB Changes:<br />\n";
 
-
+mysqli_query($dbc, "CREATE TABLE IF NOT EXISTS `field_config_ticket_security` (
+  `fieldconfigid` int(11) NOT NULL AUTO_INCREMENT,
+  `security_level` varchar(200) DEFAULT NULL,
+  `subtabs_hidden` text,
+  `subtabs_viewonly` text NOT NULL,
+  `fields_hidden` text NOT NULL,
+  `fields_viewonly` text NOT NULL,
+  PRIMARY KEY (`fieldconfigid`)
+)");
 
     echo "Dayana's DB Changes Done<br />\n";
 ?>
