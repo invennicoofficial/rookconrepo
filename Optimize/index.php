b020475e8790940a4793a20a8409ebd2d950c101
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-06-26 16:06:59 -0600

    Changes to add History

--- Optimize/index.php
+++ Optimize/index.php
@@ -37,6 +37,7 @@ if(check_subtab_persmission($dbc, 'optimize', ROLE, 'assign')) {
 if(check_subtab_persmission($dbc, 'optimize', ROLE, 'history')) {
 	$tab_list[] = 'history';
 }
+$tab_list[] = 'assign_past_imports';
 if(empty($_GET['tab']) || !in_array($_GET['tab'],$tab_list)) {
 	$_GET['tab'] = $tab_list[0];
 }
@@ -79,6 +80,10 @@ foreach(explode('#*#',get_config($dbc, 'upload_macros')) as $macro) {
 					include('field_config.php');
 				} else if($_GET['tab'] == 'macros') {
 					include('macro.php');
+				} else if($_GET['tab'] == 'history') {
+					include('history.php');
+				} else if($_GET['tab'] == 'assign_past_imports') {
+					include('past_work_orders.php');
 				} else if($_GET['tab'] == 'assign') {
 					include('assign.php');
 				} else {
