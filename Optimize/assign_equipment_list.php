02ce6965ded7448f7edfed9c6e86a9031def35ac
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-06-26 15:40:04 -0600

    Fix missing Equipment

--- Optimize/assign_equipment_list.php
+++ Optimize/assign_equipment_list.php
@@ -1,10 +1,10 @@
 <?php include('../include.php');
-$region = filter_var(($_GET['region'] != '' ? $_GET['region'] : ''),FILTER_SANITIZE_STRING);
-$location = filter_var(($_GET['location'] != '' ? $_GET['location'] : ''),FILTER_SANITIZE_STRING);
-$classification = filter_var(($_GET['classification'] != '' ? $_GET['classification'] : ''),FILTER_SANITIZE_STRING);
+$region = filter_var(($_GET['region'] != '' ? $_GET['region'] : '%'),FILTER_SANITIZE_STRING);
+$location = filter_var(($_GET['location'] != '' ? $_GET['location'] : '%'),FILTER_SANITIZE_STRING);
+$classification = filter_var(($_GET['classification'] != '' ? $_GET['classification'] : '%'),FILTER_SANITIZE_STRING);
 $date = filter_var(($_GET['date'] != '' ? $_GET['date'] : date('Y-m-d')),FILTER_SANITIZE_STRING);
 
-$equipment_list = $dbc->query("SELECT `equipment`.`equipmentid`, CONCAT(`equipment`.`category`,': ',`equipment`.`make`,' ',`equipment`.`model`,' ',`equipment`.`unit_number`) `label`, SUM(IF(`schedule`.`to_do_date`='$date',1,0)) `assigned` FROM `equipment` LEFT JOIN (SELECT IFNULL(`ticket_schedule`.`equipmentid`,`tickets`.`equipmentid`) `equipmentid`, IFNULL(`ticket_schedule`.`to_do_date`,`tickets`.`to_do_date`) `to_do_date` FROM `tickets` LEFT JOIN `ticket_schedule` ON `tickets`.`ticketid`=`ticket_schedule`.`ticketid` WHERE `tickets`.`deleted`=0 AND IFNULL(`ticket_schedule`.`deleted`,0)=0) `schedule` ON `equipment`.`equipmentid`=`schedule`.`equipmentid` WHERE '$region' IN (`equipment`.`region`,'') AND '$location' IN (`equipment`.`location`,'') AND '$classification' IN (`classification`,'') AND `equipment`.`deleted`=0 GROUP BY `equipment`.`equipmentid` ORDER BY `equipment`.`make`, `equipment`.`model`, `equipment`.`unit_number`");
+$equipment_list = $dbc->query("SELECT `equipment`.`equipmentid`, CONCAT(`equipment`.`category`,': ',`equipment`.`make`,' ',`equipment`.`model`,' ',`equipment`.`unit_number`) `label`, SUM(IF(`schedule`.`to_do_date`='$date',1,0)) `assigned` FROM `equipment` LEFT JOIN (SELECT IFNULL(`ticket_schedule`.`equipmentid`,`tickets`.`equipmentid`) `equipmentid`, IFNULL(`ticket_schedule`.`to_do_date`,`tickets`.`to_do_date`) `to_do_date` FROM `tickets` LEFT JOIN `ticket_schedule` ON `tickets`.`ticketid`=`ticket_schedule`.`ticketid` WHERE `tickets`.`deleted`=0 AND IFNULL(`ticket_schedule`.`deleted`,0)=0) `schedule` ON `equipment`.`equipmentid`=`schedule`.`equipmentid` WHERE CONCAT('*#*',IFNULL(`equipment`.`region`,''),'*#*') LIKE '%*#*$region*#*%' AND CONCAT('*#*',IFNULL(`equipment`.`location`,''),'*#*') LIKE '%*#*$location*#*%' AND CONCAT('*#*',IFNULL(`equipment`.`classification`,''),'*#*') LIKE '%*#*$classification*#*%' AND `equipment`.`deleted`=0 GROUP BY `equipment`.`equipmentid` ORDER BY `equipment`.`make`, `equipment`.`model`, `equipment`.`unit_number`");
 echo '<h3>Equipment</h3>';
 if($equipment_list->num_rows > 0) {
 	while($equip = $equipment_list->fetch_assoc()) { ?>
