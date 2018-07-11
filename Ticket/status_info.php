4a9825dd5ec3eeb5f0ddd0068ec99cf2882e0e36
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-07-11 16:24:39 -0600

    Add Logo

--- Ticket/status_info.php
+++ Ticket/status_info.php
@@ -1,7 +1,8 @@
 <?php $guest_access = true;
 include('../include.php');
 ob_clean();
-// $driver_cookie = 'SID=NAbux0yq06CxXFbtfj1V5o1TZW929aErEagLP8imQm3a0cyWd9qFqyOLB-V3E9Lqyj8Txg.; HSID=Aeb7Pr2mEk78e88jj; SSID=AYc24JQ7nCVLuE5ir; APISID=a3mNHvo5BRBJclzA/AUup9ldedWDDtpt3X; SAPISID=0MxjxCG4Y0GdXBCW/A-s28sp5FYbNPKNbc; 1P_JAR=2018-7-10-21; NID=134=otPqw16TaN-FOW6ZrbtUfU_hK1rqbpFzJSPmpXP7tO9egd_mA428oFDG2XDb6hQvbixq3B_FPr-yCWqSNWkdoVVN5YqgUFPRCIciXD6g_XAL0tDe667cmqoXhge-gSKMn_13Rw6exMmv3Ry5HVJ12fmfqI_ZBUBAymUhCNRf4-3ofMV1JMTL2SEuLSvzKZ_C5e-1G5pIuGurm2sKLNWr0Oh9ynWLDIMH-86EnpSxsmtbYMwxMftD; SIDCC=AEfoLeYgi3zNkwg7eFQEpWnf-yoUer8eJ-jMKpHjhVprMRZlcd82YxDqMBCGK_aUlUKFH0m-5Sk';
+// echo '6 Min#*#50.989#*#-114.06652';
+// exit();
 $driver_cookie = $_POST['driver'];
 if(empty($driver_cookie)) {
 	echo 'UNKNOWN#*#status_error.php?err='.urlencode('No Driver Location Found');
