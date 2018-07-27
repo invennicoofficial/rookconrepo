b36b804c03dfcadabb38460615f1bb6e1a4a0113
Dayana <dayanapatel@freshfocusmedia.com>
2018-07-27 10:54:26 -0700

    8452

--- Security/assign_privileges.php
+++ Security/assign_privileges.php
@@ -11,6 +11,11 @@
 
 <script>
 	$(document).on('change', 'select[name="role[]"]', function() { changeRole(this); });
+	$(document).on('change', 'select[name="regions[]"]', function() { changeRegions(this); });
+	$(document).on('change', 'select[name="locations[]"]', function() { changeLocations(this); });
+	$(document).on('change', 'select[name="classifications[]"]', function() { changeClassifications(this); });
+	$(document).on('change', 'select[name="positions[]"]', function() { changePositions(this); });
+
 	function changeRole(sel) {
 		var role	= $(sel).val().join(',');
 		var id		= sel.id;
@@ -18,6 +23,61 @@
 		$.ajax({
 			type: "GET",
 			url: "security_ajax_all.php?fill=change_role&role="+role+"&id="+id,
+			dataType: "html",
+			success: function(response){
+				location.reload();
+			}
+		});
+	}
+
+	function changeRegions(sel) {
+		var regions	= $(sel).val().join(',');
+		var id		= sel.id;
+		$.ajax({
+			type: "GET",
+			url: "security_ajax_all.php?fill=change_regions&regions="+regions+"&id="+id,
+			dataType: "html",
+			success: function(response){
+				location.reload();
+			}
+		});
+	}
+
+	function changeLocations(sel) {
+		var locations	= $(sel).val().join(',');
+		var id		= sel.id;
+
+		$.ajax({
+			type: "GET",
+			url: "security_ajax_all.php?fill=change_locations&locations="+locations+"&id="+id,
+			dataType: "html",
+			success: function(response){
+				location.reload();
+			}
+		});
+	}
+
+	function changeClassifications(sel) {
+		var classifications	= $(sel).val().join(',');
+		var id		= sel.id;
+
+		$.ajax({
+			type: "GET",
+			url: "security_ajax_all.php?fill=change_classifications&classifications="+classifications+"&id="+id,
+			dataType: "html",
+			success: function(response){
+				location.reload();
+			}
+		});
+	}
+
+	function changePositions(sel) {
+		var positions	= $(sel).val().join(',');
+		var id		= sel.id;
+
+		$.ajax({
+			type: "GET",
+			url: "security_ajax_all.php?fill=change_positions&positions="+positions+"&id="+id,
 			dataType: "html",
 			success: function(response){
 				location.reload();
@@ -39,7 +99,7 @@
         $notes = mysqli_fetch_assoc(mysqli_query($dbc, "SELECT `note` FROM `notes_setting` WHERE `tile`='security' AND `subtab`='security_assign_previleges_active'"));
         $note = $notes['note'];
     }
-    
+
     if ( !empty($note) ) { ?>
         <div class="notice double-gap-bottom popover-examples">
             <div class="col-sm-1 notice-icon"><img src="../img/info.png" class="wiggle-me" width="25"></div>
@@ -50,7 +110,7 @@
             <div class="clearfix"></div>
         </div><?php
     }
-    
+
 	// Pagination Config
 	$rowsPerPage = 25;
 	$pageNum = 1;
@@ -95,9 +155,9 @@
 					$name = trim($row['first_name'].' '.$row['last_name']);
 				}
 				if ( empty($name) ) { $name = 'Not given'; }
-				$region_access = array_filter(explode('#*#',$row['region_access']));
-				$location_access = array_filter(explode('#*#',$row['location_access']));
-				$classification_access = array_filter(explode('#*#',$row['classification_access']));
+				$region_access = array_filter(explode(',',$row['region_access']));
+				$location_access = array_filter(explode(',',$row['location_access']));
+				$classification_access = array_filter(explode(',',$row['classification_access']));
 				$allowed_positions = array_filter(explode(',',$row['positions_allowed'])); ?>
 
 				<tr>
@@ -112,36 +172,36 @@
 					<td data-title="Effective Privileges">View</td>
 					<?php if(count($region_list) > 0) { ?>
 						<td data-title="Allowed Regions">
-							<select name="role[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Regions" class="role chosen-select-deselect form-control">
+							<select name="regions[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Regions" class="regions chosen-select-deselect form-control">
 								<option value=""></option><?php foreach($region_list as $value) { ?>
-									<option <?= empty($region_access) || in_array($value, $region_access) ? 'selected' : '' ?> value="<?= $value ?>"><?= $value ?></option>
+									<option <?= in_array($value, $region_access) ? 'selected' : '' ?> value="<?= $value ?>"><?= $value ?></option>
 								<?php } ?>
 							</select>
 						</td>
 					<?php } ?>
 					<?php if(count($location_list) > 0) { ?>
 						<td data-title="Allowed Locations">
-							<select name="role[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Locations" class="role chosen-select-deselect form-control">
+							<select name="locations[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Locations" class="locations chosen-select-deselect form-control">
 								<option value=""></option><?php foreach($location_list as $value) { ?>
-									<option <?= empty($location_access) || in_array($value, $location_access) ? 'selected' : '' ?> value="<?= $value ?>"><?= $value ?></option>
+									<option <?= in_array($value, $location_access) ? 'selected' : '' ?> value="<?= $value ?>"><?= $value ?></option>
 								<?php } ?>
 							</select>
 						</td>
 					<?php } ?>
 					<?php if(count($classification_list) > 0) { ?>
 						<td data-title="Allowed Classifications">
-							<select name="role[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Classifications" class="role chosen-select-deselect form-control">
+							<select name="classifications[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Classifications" class="classifications chosen-select-deselect form-control">
 								<option value=""></option><?php foreach($classification_list as $value) { ?>
-									<option <?= empty($classification_access) || in_array($value, $classification_access) ? 'selected' : '' ?> value="<?= $value ?>"><?= $value ?></option>
+									<option <?= in_array($value, $classification_access) ? 'selected' : '' ?> value="<?= $value ?>"><?= $value ?></option>
 								<?php } ?>
 							</select>
 						</td>
 					<?php } ?>
 					<?php if(count($position_list) > 0) { ?>
 						<td data-title="Allowed Positions">
-							<select name="role[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Positions" class="role chosen-select-deselect form-control">
+							<select name="positions[]" multiple id="<?= $row['contactid'] ?>" data-placeholder="Select Allowed Positions" class="positions chosen-select-deselect form-control">
 								<option value=""></option><?php foreach($position_list as $value) { ?>
-									<option <?= empty($allowed_positions) || in_array($value['position_id'], $allowed_positions) ? 'selected' : '' ?> value="<?= $value['position_id'] ?>"><?= $value['name'] ?></option>
+									<option <?= in_array($value['position_id'], $allowed_positions) ? 'selected' : '' ?> value="<?= $value['position_id'] ?>"><?= $value['name'] ?></option>
 								<?php } ?>
 							</select>
 						</td>
