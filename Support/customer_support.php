b7566861c9e524251b4e9fe8f32486fd014d3ea3
Jonathan Hurdman <jonathanhurdman@freshfocusmedia.com>
2018-07-30 14:39:35 -0600

    Support Connection Error

--- Support/customer_support.php
+++ Support/customer_support.php
@@ -1,6 +1,6 @@
 <?php include('../include.php');
-// $dbc_support = mysqli_connect('localhost', 'ffm_rook_user', 'mIghtyLion!542', 'ffm_rook_db');
-$dbc_support = mysqli_connect('localhost', 'root', 'FreshFocus007', 'local_1_rook');
+$dbc_support = mysqli_connect('localhost', 'ffm_rook_user', 'mIghtyLion!542', 'ffm_rook_db');
+// $dbc_support = mysqli_connect('localhost', 'root', 'FreshFocus007', 'local_1_rook');
 $user = get_config($dbc, 'company_name');
 $url = WEBSITE_URL;
 $user_name = $user;
