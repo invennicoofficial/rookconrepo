2db676521dfccef30e89f92f79fac5bd985233bf
Baldwin <baldwinyu@freshfocusmedia.com>
2018-08-02 09:55:55 -0600

    CDS: Trip Optimizer Best Buy Macro

--- Optimize/optimize_ajax.php
+++ Optimize/optimize_ajax.php
@@ -4,6 +4,9 @@ ob_clean();
 if($_GET['action'] == 'add_macro') {
 	set_config($dbc, 'upload_macros', filter_var(implode('#*#',$_POST['value']),FILTER_SANITIZE_STRING));
 }
+if($_GET['action'] == 'bb_macro_warehouse_assignments') {
+	set_config($dbc, 'bb_macro_warehouse_assignments', filter_var(implode('#*#',$_POST['value']),FILTER_SANITIZE_STRING));
+}
 else if($_GET['action'] == 'lock') {
 	$region = filter_var($_POST['region'],FILTER_SANITIZE_STRING);
 	if($region != '') {
@@ -24,13 +27,16 @@ else if($_GET['action'] == 'assign_ticket') {
 	$id_field = filter_var($_POST['id_field'],FILTER_SANITIZE_STRING);
 	$id = filter_var($_POST['id'],FILTER_SANITIZE_STRING);
 	$date = filter_var($_POST['date'],FILTER_SANITIZE_STRING);
+	$default_status = get_config($dbc, 'ticket_default_status');
 	$ticketid = $dbc->query("SELECT `ticketid` FROM `$table` WHERE `$id_field`='$id'")->fetch_array()[0];
 	$max_start = $dbc->query("SELECT MAX(`to_do_start_time`) FROM (SELECT `to_do_start_time` FROM `ticket_schedule` WHERE `equipmentid`='$equipmentid' AND `to_do_date`='$date' AND `deleted`=0 UNION SELECT `to_do_start_time` FROM `tickets` WHERE `equipmentid`='$equipmentid' AND `to_do_date`='$date' AND `deleted`=0) `times`")->fetch_array()[0];
 	if($max_start == '') {
 		$max_start = '7:00 AM';
 	}
 	$start_time = date('g:i a',strtotime($max_start.' + 1 hour'));
-	$dbc->query("UPDATE `$table` SET `to_do_date`='$date', `equipmentid`='$equipmentid', `to_do_start_time`=".($table == 'ticket_schedule' ? "IF(`scheduled_lock`='1',`to_do_start_time`,'$start_time')" : "'$start_time'")." WHERE `$id_field`='$id'");
+	$dbc->query("UPDATE `$table` SET `status`='$default_status', `to_do_date`='$date', `equipmentid`='$equipmentid', `to_do_start_time`=".($table == 'ticket_schedule' ? "IF(`scheduled_lock`='1',`to_do_start_time`,'$start_time')" : "'$start_time'")." WHERE `$id_field`='$id'");
+	//Update warehouse stop to match above
+	$dbc->query("UPDATE `ticket_schedule` SET `status`='$default_status', `to_do_date`='$date', `equipmentid`='$equipmentid' WHERE `ticketid` = '$ticketid' AND (`type` = 'warehouse' OR IFNULL(NULLIF(CONCAT(IFNULL(`ticket_schedule`.`address`,''),IFNULL(`ticket_schedule`.`city`,'')),''),'') IN (SELECT CONCAT(IFNULL(`address`,''),IFNULL(`city`,'')) FROM `contacts` WHERE `category`='Warehouses'))");
 	$dbc->query("INSERT INTO `ticket_history` (`ticketid`,`userid`,`src`,`description`) VALUES ($ticketid,".$_SESSION['contactid'].",'optimizer','".($table == 'tickets' ? TICKET_NOUN : 'Delivery (ID: '.$id.')')." assigned to be completed at $start_time on $date.')");
 }
 else if($_GET['action'] == 'assign_ticket_deliveries') {
