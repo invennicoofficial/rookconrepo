69296755a5839e94ff37677e0fbb55188f3252eb
Baldwin <baldwinyu@freshfocusmedia.com>
2018-08-09 09:43:50 -0600

    ROOK: Start Day Security #8455

--- navigation.php
+++ navigation.php
@@ -27,10 +27,10 @@ $_SERVER['page_load_info'] .= 'Nav Bar Start: '.number_format(microtime(true) - 
 				$active_ticket_buttons .= '<a class="btn brand-btn active-ticket" href="'.WEBSITE_URL.'/Ticket/index.php?'.($ticket_tile_visible ? '' : 'tile_name='.$active_ticket['ticket_type'].'&').'edit='.$active_ticket['ticketid'].'&from='.urlencode(WEBSITE_URL.$_SERVER['REQUEST_URI']).'&action_mode='.$ticket_action_mode.'">'.$label.'</a>';
 			}
 		}
-		if(SHOW_SIGN_IN == '1') {
+		if(SHOW_SIGN_IN == '1' && strpos(get_privileges($dbc, 'start_day_button', ROLE),'*hide*') === FALSE) {
 			$active_ticket_buttons .= '<a class="btn brand-btn active-ticket" href="'.WEBSITE_URL.'/Timesheet/start_day.php">'.END_DAY.'</a>';
 		}
-	} else if(SHOW_SIGN_IN == '1' || ACTIVE_DAY_BANNER != '') {
+	} else if((SHOW_SIGN_IN == '1' || ACTIVE_DAY_BANNER != '') && strpos(get_privileges($dbc, 'start_day_button', ROLE),'*hide*') === FALSE) {
 		$timer_running = mysqli_fetch_assoc(mysqli_query($dbc, "SELECT `timer_start` FROM `time_cards` WHERE `type_of_time` IN ('day_tracking','day_break') AND `timer_start` > 0 AND `staff`='".$_SESSION['contactid']."'"))['timer_start'];
 		if(ACTIVE_DAY_BANNER != '' && $timer_running > 0) {
 			$active_ticket_buttons .= '<a class="btn brand-btn active-ticket" href="'.WEBSITE_URL.'/Timesheet/start_day.php">'.\ACTIVE_DAY_BANNER.'</a>';
