6d398a94d1ca6bd16eb281db1aebb0ca053d2e26
Jay <jaylahiru@freshfocusmedia.com>
2018-08-15 12:34:36 -0600

    Reports: Mobile Submit Reopens Tab and Report + Create Invoice Layout Update Ticket 8719 ROOK

--- Reports/report_tiles.php
+++ Reports/report_tiles.php
@@ -17,11 +17,29 @@ if($_GET['mobile_view'] == 'true') {
 				$('.tile-sidebar,.main-screen.standard-body').height(view_height);
 			}).resize();
 		} else {
+            /* 'landing' query variable is set on tile_data.php
+             * It queries the default report set in Settings > Default Report
+             */
             <?php if ( isset($_GET['landing']) && $_GET['landing']=='true' ) { ?>
                 var type = '<?= $_GET['type'] ?>';
                 var report = '<?= $_GET['report'] ?>';
-                //var default_report = '<?= '../'.$_SERVER['REQUEST_URI'] ?>';
                 loadDefaultReport(type, report);
+            <?php } ?>
+            
+            // Search within report on mobile
+            <?php if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) { ?>
+                var type = '<?= $_POST['report_type'] ?>';
+                var report = '<?= $_GET['report'] ?>';
+                var data = {
+                    <?php
+                        $post_data = [];
+                        foreach($_POST as $key => $value) {
+                            $post_data[] = $key.": '$value'";
+                        }
+                        echo $post_data = implode(',', $post_data);
+                    ?>
+                }
+                loadSearch(type, report, data);
             <?php } ?>
         }
 		$('#mobile_tabs .panel-heading').click(loadPanel);
@@ -49,6 +67,23 @@ if($_GET['mobile_view'] == 'true') {
 		$.ajax({
 			url: panel.data('file-name')+'&report='+report,
 			method: 'POST',
+			response: 'html',
+			success: function(response) {
+				panel.html(response);
+			}
+		});
+	}
+	function loadSearch(type, report, data) {
+        var panel = $('#collapse_'+type+' .panel-body');
+        panel.closest('.panel-collapse').addClass('in');
+        panel.closest('.panel').find('.panel-heading').addClass('active');
+        panel.closest('.panel').find('.collapsed').removeClass('collapsed');
+        
+		panel.html('Loading...');
+		$.ajax({
+			url: panel.data('file-name')+'&report='+report,
+			method: 'POST',
+            data: data,
 			response: 'html',
 			success: function(response) {
 				panel.html(response);
